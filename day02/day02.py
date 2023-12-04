6543389c13e98ff7f89ca05c58f0a66426455558
Sorsell <Sorsell@users.noreply.github.com>
2023-12-04 14:54:35 +0100

    day2 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,4 @@
-def part1(game):
-    control = {"red": 12, "green": 13, "blue": 14}
+def part1(game,control):
     game = game.split(":")
     id = int(game[0].split(" ")[1])
     sets = game[1].split(";")
@@ -13,12 +12,34 @@ def part1(game):
                 return 0
     return id
 
+def part2(game):
+    min = {"red": 0, "green": 0, "blue": 0}
+    game = game.split(":")
+    id = int(game[0].split(" ")[1])
+    sets = game[1].split(";")
+    for set in sets:
+        parts = set.split(",")
+        for part in parts:
+            cube = part.strip().split(" ")
+            value = int(cube[0])
+            color = cube[1]
+            if value > min[color]:
+                min[color] = value
+    sum = min["red"] * min["green"] * min["blue"]
+    return sum
                 
                 
 with open('day02\day02Input.txt') as f:
     lines = f.readlines()
 
+control = {"red": 12, "green": 13, "blue": 14}
 value = 0
 for game in lines:
-    value += part1(game)
+    value += part1(game,control)
 print(value)
+
+sum = 0
+for game in lines:
+    sum += part2(game)
+print(sum)
+

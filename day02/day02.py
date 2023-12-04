ae42d1d92e593584bed957a21d23fa03c8d6194e
Sorsell <Sorsell@users.noreply.github.com>
2023-12-04 12:34:41 +0100

    day2 part1

--- day02/day02.py
+++ day02/day02.py
@@ -1,15 +1,24 @@
-def part1(line):
-    line = line.split(":")
-    parts = line[1].split(";")
-    print(parts)
-    for part in parts:
-        parts2 = part.split(",")
-        print(parts2)
+def part1(game):
+    control = {"red": 12, "green": 13, "blue": 14}
+    game = game.split(":")
+    id = int(game[0].split(" ")[1])
+    sets = game[1].split(";")
+    for set in sets:
+        parts = set.split(",")
+        for part in parts:
+            cube = part.strip().split(" ")
+            value = int(cube[0])
+            color = cube[1]
+            if value > control[color]:
+                return 0
+    return id
+
+                
                 
 with open('day02\day02Input.txt') as f:
     lines = f.readlines()
 
 value = 0
-for line in lines:
-    value += part1(line)
+for game in lines:
+    value += part1(game)
 print(value)
